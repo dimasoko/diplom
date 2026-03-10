9915ba57724f6afe02e8c9287ba11c70f161d9f2
dmitriy <dimasokolov28022006@gmail.com>
2026-03-10 18:00:45 +0700

    create front/back prjcts

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+import { Button } from 'rizzui';
+
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <Button variant="solid">Всё работает! ☕</Button>
+    </div>
+  );
+}
